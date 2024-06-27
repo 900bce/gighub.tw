53a89467788e2cadb138b9f54216272103f9811c
jimmywen <grd741@gmail.com>
2024-06-28 03:03:06 +0800

    mock data

--- app/components/EventCard.tsx
+++ app/components/EventCard.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
+import dayjs from 'dayjs';
 
 type Props = {
   title: string;
-  date: string;
+  date: Date;
   artists: string[];
 };
 
 function EventCard({ title, date, artists }: Props) {
   return (
     <div className="m-1 p-2 border-b border-gray-300">
-      <p className='text-gray-500 text-sm'>{date}</p>
-      <h3>{title}</h3>
-      {artists.map((artist) => (
-        <p key={artist} className='text-sm text-gray-500'>{artist}</p>
-      ))}
+      <div className="flex justify-between mt-2">
+        <p className="text-gray-500 text-sm">
+          {dayjs(date).format('MM/DD (ddd) HH:mm')}
+        </p>
+        <button className="text-sm text-white bg-gray-400 px-3">購票</button>
+      </div>
+      <h3 className="text-lg font-thin py-1">{title}</h3>
+      <p>{}</p>
+      <div>
+        {artists.map((artist) => (
+          <p key={artist} className="text-sm text-gray-500">
+            {artist}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import EventCard from '@/app/components/EventCard';
 
+type Event = {
+  id: string;
+  title: string;
+  venue: string;
+  date: Date;
+  artists: string[];
+};
+
+const mockData: Event[] = [
+  {
+    id: '0001',
+    title: '拍謝少年噪音公寓厝邊倒彈演唱會',
+    venue: '台北流行音樂中心',
+    date: new Date('2024-08-24 18:00:00'),
+    artists: ['拍謝少年', '台灣通勤第一品牌'],
+  },
+  {
+    id: '0002',
+    title: '拍謝少年噪音公寓厝邊倒彈演唱會',
+    venue: '高雄流行音樂中心',
+    date: new Date('2024-08-25 19:30:00'),
+    artists: ['拍謝少年', '台灣通勤第一品牌'],
+  }
+];
+
 function page() {
   return (
     <>
-      <EventCard
-        title="拍謝少年噪音公寓厝邊倒彈演唱會"
-        date="08/24 (六)"
-        artists={['拍謝少年']}
-      />
+      {mockData.map((event) => (
+        <EventCard
+          key={event.id}
+          title={event.title}
+          date={event.date}
+          artists={event.artists}
+        />
+      ))}
     </>
   );
 }
