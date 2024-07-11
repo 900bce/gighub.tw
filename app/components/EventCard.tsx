13860547682b2a41655761b7cd13fc2403016c52
jimmywen <grd741@gmail.com>
2024-07-12 02:50:41 +0800

    update EventCard style

--- app/components/EventCard.tsx
+++ app/components/EventCard.tsx
@@ -17,12 +17,12 @@ function EventCard({ title, date, artists, venue }: Props) {
       </div>
       <h3 className="text-lg font-medium text-gray-600">{title}</h3>
       <p className="pb-1 text-sm text-gray-400">{venue}</p>
-      <div className="border-b pb-5">
+      <div className="border-b pb-5 flex justify-center flex-wrap">
         {artists.map((artist, index) => (
-          <span key={artist} className="text-sm text-gray-500">
+          <div key={artist} className="text-sm text-gray-500">
             {artist}
             {index < artists.length - 1 && ','}
-          </span>
+          </div>
         ))}
       </div>
     </div>
